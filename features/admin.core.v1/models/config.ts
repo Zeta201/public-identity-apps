0b908cdc3a2a60bd0a743a273633d8c6259a2755
Dinitha Wijewardhana <dini2040gh@gmail.com>
2026-06-25 00:36:30 +0530

    Add UI implementation for OpenID4VP

--- features/admin.core.v1/models/config.ts
+++ features/admin.core.v1/models/config.ts
@@ -929,6 +929,7 @@ export interface ServiceResourceEndpointsInterface extends AgentsResourceEndpoin
     ConsentMgtResourceEndpointsInterface {
 
     CORSOrigins: string;
+    vpTemplates: string;
     copilot: string;
     // TODO: Remove this endpoint and use ID token to get the details
     me: string;
